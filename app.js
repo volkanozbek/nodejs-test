95695900567db11812d983b24f3ebdb56203ea72
Volkan Özbek <volkanozbk@hotmail.com>
2020-05-03 17:50:09 +0300

    dev

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var fs = require('fs');
 var express = require('express');
 var path = require('path');
 var app = express();
+var ejsLayouts = require('express-ejs-layouts');
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './app/views/'));
 
 var testController = require('./app/routes/nodeRoute');
 
-
+app.use(ejsLayouts);
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use('/test', testController);
--- app/controller/nodeController.js
+++ app/controller/nodeController.js
@@ -1,7 +1,10 @@
 var path = require('path');
 
 module.exports.index = function(req, res){
-    res.sendFile(path.join(__dirname, '../../index.html'));
+    var kisiler = ['Volkan','Ozbek'];
+    
+    res.render('index', {mesaj: 'Mesaj controller'});
+    
 }
 
 module.exports.test = function(req, res){
