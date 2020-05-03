4f684434000daa02f70c736da3c03cf8bd9a5e08
Volkan Özbek <volkanozbk@hotmail.com>
2020-05-03 22:59:51 +0300

    dev

--- app/controller/nodeController.js
+++ app/controller/nodeController.js
@@ -1,14 +1,12 @@
 var path = require('path');
 
 module.exports.index = function(req, res){
-    var kisiler = ['Volkan','Ozbek'];
-    
-    res.render('index', {mesaj: 'Mesaj controller'});
+    res.render('index');
     
 }
 
 module.exports.test = function(req, res){
-    res.sendFile(path.join(__dirname, '../../login.html'));
+    res.render('onepage');
 }
 
 
