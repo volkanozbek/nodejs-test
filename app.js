0bc53fa5e73ac14e42f117eaf32471aa9f83b17f
Volkan Özbek <volkanozbk@hotmail.com>
2020-05-03 10:37:33 +0300

    first update

---
+++ app.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+var express = require('express');
+var path = require('path');
+var testController = require('./nodeController');
+
+var app = express();
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', testController);
+
+app.listen(8080);
---
+++ nodeController.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var express = require('express');
+var router = express.Router();
+
+module.exports.index = function(req, res){
+    res.sendFile(path.join(__dirname, 'index.html'));
+}
+module.exports.test = function(req, res){
+    res.sendFile(path.join(__dirname, 'login.html'));
+}
+
+
+router.get('/test', module.exports.index);
+router.get('/test/one', module.exports.test);
+
+module.exports = router;
